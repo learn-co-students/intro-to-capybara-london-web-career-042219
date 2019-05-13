fcf998d4d9f39022467006d0e474ea09233e37b4
MarcusMellorMIM <marcus@mellor.com>
2019-05-13 15:59:28 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,11 @@
 class Application < Sinatra::Base
   # Write your code here!
+    get '/' do
+     erb :index
+    end
 
+    post '/greet' do
+      @user_name=params[:user_name]
+      erb :greet
+    end
 end
